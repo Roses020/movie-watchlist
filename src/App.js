8c6dad1b40efbad2503cb66da2c824cef59fd126
Aimee <aimeeray3@gmail.com>
2022-12-23 14:19:41 -0700

    movie watchlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Header from "./Components/Header";
+import MovieScreen from "./Components/MovieScreen";
+import Watchlist from "./Components/Watchlist";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [watchlist, setWatchList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [list, setList] = useState([]);
+
+  function addMovie(movie) {
+    setList([...list, movie]);
+  }
+  function removeMovie(movie){
+    const newState = list.filter((mov) => {
+      return mov !== (movie);
+
+    });
+    
+    setList(newState);
+  };
+
+  const getData = () => {
+    axios
+      .get(
+ `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        setMovieList(res.data.results);
+        //console.log(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header />
+      <main>
+        <MovieScreen
+        removeMovie={removeMovie}
+         page={page}
+         setPage={setPage}
+         movieList={movieList}
+         watchlist={watchlist}
+         addMovie={addMovie}
+         />
+       <Watchlist list={list} removeMovie={removeMovie}/>
+       </main>
+     </div>
   );
 }
 
